7d85ec505d718d9d11696825ed7180df60d0ba1e
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2022-09-16 14:57:44 +0300

    ilk ödev

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,17 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=12;
+		sayi=13;
+		
+		byte sayi2=127;
+		
+		String sehir="Ankara";
+		char karakter='a';
+		
+		boolean dogruMu=true;
+	}
+
+}
---
+++ javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,10 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba java");
+		System.out.println("Merhaba java2");
+	}
+
+}
